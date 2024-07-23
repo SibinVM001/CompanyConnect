e75615493c1031ef4cee7810639914252c01bc67
Sibin <sibin.limenzy@gmail.com>
2024-07-23 12:02:30 +0530

    validation

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\{Auth, Validator};
 
 class LoginController extends Controller
 {    
@@ -16,25 +16,31 @@ class LoginController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
 
-        if (Auth::attempt($request->only('email', 'password'))) {
-            $user = Auth::user();
+        if ($validator->fails()) {
+            return response()->json(['Invalid Data', $validator->errors()->toArray()]);
+        } else {
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user = Auth::user();
 
-            if ($user->is_admin) {
-                $token = $user->createToken('authToken')->plainTextToken;
+                if ($user->is_admin) {
+                    $token = $user->createToken('authToken')->plainTextToken;
 
-                return response()->json(['token' => $token], 200);
+                    return response()->json(['token' => $token], 200);
+                } else {
+                    Auth::logout();
+
+                    return response()->json(['error' => 'You are not authorized to access this Api'], 401);
+                }
             } else {
-                Auth::logout();
-
-                return response()->json(['error' => 'You are not authorized to access this Api'], 401);
+                return response()->json(['success' => 'Invalid credentials'], 200);
             }
-        }
 
-        return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
     }
 }
